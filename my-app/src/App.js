901715ce264dce1d21c168ec5dd2313f3894f1ad
valdecir <valdecir.neper@gmail.com>
2024-06-07 02:13:38 +0000

    setting up an app

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,46 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Home from "./components/examples/Home";
+import About from "./components/examples/About";
+import Contact from "./components/examples/Contact";
 
-const App = () => {
-  return <div></div>;
-};
+function App() {
+  return (
+
+    <div>
+      <h1>Welcome to my React App!</h1>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/contact">Contact</Link>
+              </li>
+            </ul>
+          </nav>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/about" element={<About />} />
+            <Route exact path="/contact" element={<Contact />} />
+
+
+          </Routes>
+
+
+        </div>
+
+      </Router>
+    </div>
+
+  );
+
+}
 
 export default App;
--- my-app/src/components/examples/Contact.js
+++ my-app/src/components/examples/Contact.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Contact = () => {
-  return <></>;
+  return <>Contact</>;
 };
 
 export default Contact;
